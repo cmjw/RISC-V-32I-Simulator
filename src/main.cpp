d800917c5cd24058f96ec99015795f84c5246060
Charlotte <cmjwfeltwell@gmail.com>
2025-07-01 17:46:07 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include "mem.h"
+
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
     printf("RISC-V 32I Simulator\nCharlotte White\n");
+
+    Memory memory;
+    
+    memory.memory_dump(0);
+
+    memory.store_word(0x0, 0x12345678);
+
+    memory.memory_dump(0);
+
+    printf("Word: %02X\n", memory.load_word(0x0));
+
     return 0;
 }
